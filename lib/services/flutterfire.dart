5bc65969ac4702b9de7220546b89a56ddfc56afb
kartik1081 <81360720+kartik1081@users.noreply.github.com>
2021-05-29 22:49:06 +0530

    Day 2

--- lib/services/flutterfire.dart
+++ lib/services/flutterfire.dart
@@ -1,14 +1,25 @@
+import 'dart:js';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:training/main.dart';
 import 'package:training/modles/users.dart';
 
 class FlutterFire {
   late String name, email, password, imagePath;
   FirebaseAuth _auth = FirebaseAuth.instance;
   FirebaseDatabase _database = FirebaseDatabase.instance;
-  Future<void> singIn() async {
+  Future<void> singIn(BuildContext context) async {
     try {
       await _auth.signInWithEmailAndPassword(email: email, password: password);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => new MyApp(),
+        ),
+      );
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
@@ -18,7 +29,7 @@ class FlutterFire {
     }
   }
 
-  Future<void> signUp() async {
+  Future<void> signUp(BuildContext context) async {
     try {
       await _auth
           .createUserWithEmailAndPassword(email: email, password: password)
@@ -27,6 +38,12 @@ class FlutterFire {
             name: name, email: email, password: password, imagePath: imagePath);
         String id = _auth.currentUser!.uid;
         _database.reference().child("Users").child("$id").set(users);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => new MyApp(),
+          ),
+        );
       });
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
